da82362d85fd79fd1f7af2fce3c30a05953f9482
Gunjan <gunjan9861@gmail.com>
2024-01-02 17:23:09 +0545

    Weather app and its code

---
+++ localStorage/app.js
@@ -0,0 +1,10 @@
+localStorage.setItem('name','Gunjan')
+localStorage.setItem('age',21)
+
+
+//get items from local storage
+
+let name = localStorage.getItem('name')
+let age = localStorage.getItem('age')
+
+console.log(name,age)
